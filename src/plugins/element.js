c633b3bf524bee1257e6f31d9855aecb8931deb6
zqj <2367408884@qq.com>
2021-04-18 13:25:57 +0800

    编写row的scss

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,5 @@
 import Element from "../components/element";
-import "../assets/theme-chalk/element/index.scss";
+import "../assets/theme-chalk/index.scss";
 import Vue from "vue";
 
 Vue.use(Element);
